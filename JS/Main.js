cba182c937381952be04d4b24b2673b546fafdbf
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-11-15 18:54:45 +0300

    Update Main.js

--- JS/Main.js
+++ JS/Main.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     let topButton = document.getElementById("topButton");
-    window.onscroll = () => {   
+    window.onscroll = () => {    
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             topButton.style.display = "block";
         } else {
